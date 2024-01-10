38153d70f399d65154e1ce5abc8451fed190cc6c
Hanzalashaik <shaikhhanzala27@gmail.com>
2024-01-10 08:24:11 +0000

    Completed✅

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url); //
 const __dirname = path.dirname(__filename); //
 const app = express();
 
-const port = 5000;
+const port = 5006;
 
 app.use(express.static(path.join(__dirname, "dist")));
 app.get("/*", (req, res) => {
